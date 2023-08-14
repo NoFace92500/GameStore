d8f4730b7a2db34d457011d46e29edccd1b25922
Ray Bilodeau <rbil@DESKTOP-B5KI19D>
2023-08-14 17:06:11 -0400

    intial commit

---
+++ public/videoGameDisplay.js
@@ -0,0 +1,17 @@
+//get the ul element
+const ul = document.querySelector('ul');
+
+//GET array of students from localhost:8080/api/v1/students
+fetch('http://localhost:8080/api/v1/videoGames')
+    .then(response => response.json())
+    .then(videoGames => {
+        //loop through the array of students
+        videoGames.forEach(videoGame => {
+            //create a new li element
+            const li = document.createElement('li');
+            //set the text of the li element to the student's name
+            li.textContent = videoGame.name;
+            //append the li element to the ul element
+            ul.appendChild(li);
+        });
+    });
